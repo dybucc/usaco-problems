41d122b9bc416dcada4dc9e99a99faca37a8f396
dybucc <149513579+dybucc@users.noreply.github.com>
2025-05-17 17:32:20 +0200

    feat: finish os2025/bronze/problem1

--- src/problems/open_shot_2025/bronze/problem1.rs
+++ src/problems/open_shot_2025/bronze/problem1.rs
@@ -1,8 +1,6 @@
-#![allow(unused)]
-
 use std::collections::HashMap;
 
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 enum Outcome {
     Draw,
     Win,
@@ -30,21 +28,12 @@ impl From<&u8> for Outcome {
     }
 }
 
-#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
 struct Input(usize, usize);
 
 impl Input {
     fn new(i: usize, j: usize) -> Self {
         Self(i, j)
-    }
-
-    fn combine(&self, other: &Self) -> [Input; 4] {
-        [
-            Self(self.0, other.0),
-            Self(self.1, other.1),
-            Self(self.0, other.1),
-            Self(self.1, other.0),
-        ]
     }
 }
 
@@ -56,37 +45,30 @@ impl Comparator for Vec<Input> {
     fn compare(&self, counter: &Self, rules: HashMap<Input, Outcome>) -> Vec<u32> {
         let mut output = vec![0; counter.len()];
 
-        // for each counter from elsie, analyze each individual number/hoof
-        // the analysis should check if the possibility can win against it for at least one of them
-        // so e.g. if elsie comes up with 2 3, one possibility is to come up with 3 3 as a counter
-        // let's analyze it this way
-        // for 2 in 2 3 against 3 3 => can 3 win against 2? yes, can 3 win against 2, yes
-        // for 3 in 2 3 against 3 3 => can 3 win against 3? no, can 3 win against 3? no
-        // in each one of the previous assertions, there ought to be at least one yes for 3 3 to be
-        // a winning combination
-
         for (idx, i) in counter.iter().enumerate() {
-            let x = i.0;
-            let y = i.1;
+            let hoof1 = i.0;
+            let hoof2 = i.1;
 
             for j in self {
                 let mut check1 = false;
                 let mut check2 = false;
 
-                if *rules.get(&Input::new(j.0, x)).unwrap() == Outcome::Win
-                    || *rules.get(&Input::new(j.1, x)).unwrap() == Outcome::Win
+                if *rules.get(&Input::new(j.0, hoof1)).unwrap() == Outcome::Win
+                    && *rules.get(&Input::new(j.0, hoof2)).unwrap() == Outcome::Win
                 {
                     check1 = true;
                 }
 
-                if *rules.get(&Input::new(j.0, y)).unwrap() == Outcome::Win
-                    || *rules.get(&Input::new(j.1, y)).unwrap() == Outcome::Win
+                if *rules.get(&Input::new(j.1, hoof1)).unwrap() == Outcome::Win
+                    && *rules.get(&Input::new(j.1, hoof2)).unwrap() == Outcome::Win
                 {
                     check2 = true;
                 }
 
-                if check1 && check2 {
+                if check1 || check2 {
                     *output.get_mut(idx).unwrap() += 1;
+                    dbg!(j.0);
+                    dbg!(j.1);
                 }
             }
         }
@@ -98,7 +80,7 @@ fn process_input(input: &str) -> (HashMap<Input, Outcome>, Vec<Input>, Vec<Input
     let (symbols, input) = input.split_at(input.find(' ').unwrap());
     let symbols: usize = symbols.parse().unwrap();
     let (games, input) = input.split_at(input.find('\n').unwrap());
-    let games = games.parse().unwrap();
+    let games = games.trim().parse().unwrap();
     let (relations, input) = input.split_at(input.find(|v: char| v.is_numeric()).unwrap());
 
     let mut map: HashMap<Input, Outcome> = HashMap::with_capacity(symbols);
@@ -112,7 +94,6 @@ fn process_input(input: &str) -> (HashMap<Input, Outcome>, Vec<Input>, Vec<Input
             map.insert(Input::new(i + 1, j + 1), dummy);
             possibilities.push(Input::new(i + 1, j + 1));
 
-            // for e.g. 2 1, 2 wins but 1 loses, so 1 2 should have 1 losing and 2 winning
             if i != j {
                 map.insert(Input::new(j + 1, i + 1), dummy.rev());
                 possibilities.push(Input::new(j + 1, i + 1));
@@ -123,10 +104,16 @@ fn process_input(input: &str) -> (HashMap<Input, Outcome>, Vec<Input>, Vec<Input
     for line in input.lines() {
         let (i, line) = line.split_at(line.find(' ').unwrap());
         let i: usize = i.parse().unwrap();
-        let j: usize = line.parse().unwrap();
+        let j: usize = line.trim().parse().unwrap();
 
         counter.push(Input::new(i, j));
     }
 
     (map, possibilities, counter)
+}
+
+pub fn process(input: &str) -> Vec<u32> {
+    let (rules, besie, elsie) = process_input(input);
+
+    besie.compare(&elsie, rules)
 }
